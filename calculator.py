a8a0944d3f3bc7ac87166a936340a695b515f6fb
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-09 19:51:06 -0700

    Added all airthmetics in the calculator.py file

--- calculator.py
+++ calculator.py
@@ -5,3 +5,38 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+input_string = input("")
+tokens = input_string.split(' ')
+
+while True:
+  if tokens[0] == 'pow':
+    print(power(num1 = float(tokens[1]), num2 = float(tokens[2])))
+    break
+
+  elif tokens[0] == 'add':
+    print(add(num1 = float(tokens[1]), num2 = float(tokens[2])))
+    break
+
+  elif tokens[0] == 'subtract':
+    print(subtract(num1 = float(tokens[1]), num2 = float(tokens[2])))
+    break
+
+  elif tokens[0] == 'multiply':
+    print(multiply(num1 = float(tokens[1]), num2 = float(tokens[2])))
+    break
+
+  elif tokens[0] == 'divide':
+    print(divide(num1 = float(tokens[1]), num2 = float(tokens[2])))
+    break
+
+  elif tokens[0] == 'mod':
+    print(mod(num1 = float(tokens[1]), num2 = float(tokens[2])))
+    break
+
+  elif tokens[0] == 'square':
+    print(mod(num1 = float(tokens[1])))
+    break
+
+  elif tokens[0] == 'cube':
+    print(cube(num1 = float(tokens[1])))
+    break
