be11d4d11f2389a33b90607433d758c503cb75de
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-09 20:27:06 -0700

    Added text file open for input

--- calculator.py
+++ calculator.py
@@ -5,38 +5,46 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
-input_string = input("")
-tokens = input_string.split(' ')
+# input_string = input("")
+# tokens = input_string.split(' ')
+file_name = open("data2.txt")
 
-while True:
-  if tokens[0] == 'pow':
-    print(power(num1 = float(tokens[1]), num2 = float(tokens[2])))
-    break
+for line in file_name:
+  line = line.rstrip()
+  tokens = line.split(' ')
+  # print(tokens)
 
-  elif tokens[0] == 'add':
-    print(add(num1 = float(tokens[1]), num2 = float(tokens[2])))
-    break
+  while True:
+    if tokens[0] == 'pow':
+      print(power(num1 = float(tokens[1]), num2 = float(tokens[2])))
+      break
 
-  elif tokens[0] == 'subtract':
-    print(subtract(num1 = float(tokens[1]), num2 = float(tokens[2])))
-    break
+    elif tokens[0] == 'add':
+      print(add(num1 = float(tokens[1]), num2 = float(tokens[2])))
+      break
 
-  elif tokens[0] == 'multiply':
-    print(multiply(num1 = float(tokens[1]), num2 = float(tokens[2])))
-    break
+    elif tokens[0] == 'subtract':
+      print(subtract(num1 = float(tokens[1]), num2 = float(tokens[2])))
+      break
 
-  elif tokens[0] == 'divide':
-    print(divide(num1 = float(tokens[1]), num2 = float(tokens[2])))
-    break
+    elif tokens[0] == 'multiply':
+      print(multiply(num1 = float(tokens[1]), num2 = float(tokens[2])))
+      break
 
-  elif tokens[0] == 'mod':
-    print(mod(num1 = float(tokens[1]), num2 = float(tokens[2])))
-    break
+    elif tokens[0] == 'divide':
+      print(divide(num1 = float(tokens[1]), num2 = float(tokens[2])))
+      break
 
-  elif tokens[0] == 'square':
-    print(mod(num1 = float(tokens[1])))
-    break
+    elif tokens[0] == 'mod':
+      print(mod(num1 = float(tokens[1]), num2 = float(tokens[2])))
+      break
 
-  elif tokens[0] == 'cube':
-    print(cube(num1 = float(tokens[1])))
-    break
+    elif tokens[0] == 'square':
+      print(square(num1 = float(tokens[1])))
+      break
+
+    elif tokens[0] == 'cube':
+      print(cube(num1 = float(tokens[1])))
+      break
+
+file_name.close()
